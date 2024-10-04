f654b16d521e09d3b4d277ab4f696fb9a94cbd05
rafeym <mohammadrafey0@gmail.com>
2024-10-04 16:09:43 -0400

    DEVELOP: created basic invoice form

---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,48 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const NewInvoice = () => {
+  return (
+    <main className="flex flex-col justify-center h-full  max-w-5xl mx-auto gap-6 my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-semibold">Create Invoice</h1>
+      </div>
+      <form className="flex flex-col gap-4 max-w-xs">
+        <div>
+          <Label htmlFor="name" className="block mb-2 font-semibold text-sm">
+            Billing Name
+          </Label>
+          <Input id="name" name="name" type="text" />
+        </div>
+        <div>
+          <Label htmlFor="email" className="block mb-2 font-semibold text-sm">
+            Billing Email
+          </Label>
+          <Input id="email" name="email" type="email" />
+        </div>
+        <div>
+          <Label htmlFor="value" className="block mb-2 font-semibold text-sm">
+            Value
+          </Label>
+          <Input id="value" name="value" type="number" />
+        </div>
+        <div>
+          <Label
+            htmlFor="description"
+            className="block mb-2 font-semibold text-sm"
+          >
+            Description
+          </Label>
+          <Textarea id="description" name="description" />
+        </div>
+        <div>
+          <Button className="w-full font-semibold">Submit</Button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default NewInvoice;
