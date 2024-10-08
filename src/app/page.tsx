bfdd9d9ccee5bae409cefb25d2f514e2dc1025dc
rafeym <mohammadrafey0@gmail.com>
2024-10-07 22:22:17 -0400

    DEVELOP: Added Clerk authentication to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 import { Button } from "@/components/ui/button";
 
 import Link from "next/link";
@@ -7,6 +15,12 @@ export default function Home() {
     <main className="flex flex-col justify-center h-screen text-center max-w-5xl mx-auto gap-6">
       <h1 className="text-5xl font-bold">Invoiced</h1>
       <p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <Button asChild>
           <Link href="/dashboard">Sign In</Link>
         </Button>
