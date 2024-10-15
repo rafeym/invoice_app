bc94cd221881ffe1d0b4fc733521fe8c3813f90d
rafeym <mohammadrafey0@gmail.com>
2024-10-15 12:44:03 -0400

    REFACTOR: Removed width, margin and padding to reusable container component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,85 +16,88 @@ import {
 import { CirclePlus } from "lucide-react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import Container from "@/components/Container";
 
 const Dashboard = async () => {
   const results = await db.select().from(Invoices);
   return (
-    <main className="flex flex-col justify-center h-full text-center max-w-5xl mx-auto gap-6 my-12">
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-semibold">Invoices</h1>
-        <p>
-          <Button className="inline-flex gap-2" variant="ghost" asChild>
-            <Link href="/invoices/new">
-              <CirclePlus className="h-4 w-4" /> Create invoice
-            </Link>
-          </Button>
-        </p>
-      </div>
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px] p-4">Date</TableHead>
-            <TableHead className="p-4">Customer</TableHead>
-            <TableHead className="p-4">Email</TableHead>
-            <TableHead className="text-center p-4">Status</TableHead>
-            <TableHead className="text-right p-4">Value</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {results.map((result) => {
-            return (
-              <TableRow key={result.id}>
-                <TableCell className="font-medium text-left p-0">
-                  <Link
-                    href={`/invoices/${result.id}`}
-                    className="font-semibold p-4 block"
-                  >
-                    {new Date(result.createTs).toLocaleDateString("en-US")}
-                  </Link>
-                </TableCell>
-                <TableCell className="text-left p-0">
-                  <Link
-                    href={`/invoices/${result.id}`}
-                    className="font-semibold p-4 block"
-                  >
-                    Mohammad Rafey
-                  </Link>
-                </TableCell>
-                <TableCell className="text-left p-0">
-                  <Link className="p-4 block" href={`/invoices/${result.id}`}>
-                    mrafey10@gmail.com
-                  </Link>
-                </TableCell>
-                <TableCell className="text-center p-0">
-                  <Link className="p-4 block" href={`/invoices/${result.id}`}>
-                    <Badge
-                      className={cn(
-                        "rounded-full capitalize",
-                        result.status === "open" && "bg-blue-600",
-                        result.status === "paid" && "bg-green-600",
-                        result.status === "void" && "bg-zinc-700",
-                        result.status === "uncollectible" && "bg-red-600"
-                      )}
+    <main className=" h-full">
+      <Container>
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-semibold">Invoices</h1>
+          <p>
+            <Button className="inline-flex gap-2" variant="ghost" asChild>
+              <Link href="/invoices/new">
+                <CirclePlus className="h-4 w-4" /> Create invoice
+              </Link>
+            </Button>
+          </p>
+        </div>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px] p-4">Date</TableHead>
+              <TableHead className="p-4">Customer</TableHead>
+              <TableHead className="p-4">Email</TableHead>
+              <TableHead className="text-center p-4">Status</TableHead>
+              <TableHead className="text-right p-4">Value</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {results.map((result) => {
+              return (
+                <TableRow key={result.id}>
+                  <TableCell className="font-medium text-left p-0">
+                    <Link
+                      href={`/invoices/${result.id}`}
+                      className="font-semibold p-4 block"
                     >
-                      {result.status}
-                    </Badge>
-                  </Link>
-                </TableCell>
-                <TableCell className="text-right p-0">
-                  <Link
-                    href={`/invoices/${result.id}`}
-                    className="font-semibold p-4 block"
-                  >
-                    ${result.value / 100}
-                  </Link>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+                      {new Date(result.createTs).toLocaleDateString("en-US")}
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-left p-0">
+                    <Link
+                      href={`/invoices/${result.id}`}
+                      className="font-semibold p-4 block"
+                    >
+                      Mohammad Rafey
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-left p-0">
+                    <Link className="p-4 block" href={`/invoices/${result.id}`}>
+                      mrafey10@gmail.com
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-center p-0">
+                    <Link className="p-4 block" href={`/invoices/${result.id}`}>
+                      <Badge
+                        className={cn(
+                          "rounded-full capitalize",
+                          result.status === "open" && "bg-blue-600",
+                          result.status === "paid" && "bg-green-600",
+                          result.status === "void" && "bg-zinc-700",
+                          result.status === "uncollectible" && "bg-red-600"
+                        )}
+                      >
+                        {result.status}
+                      </Badge>
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-right p-0">
+                    <Link
+                      href={`/invoices/${result.id}`}
+                      className="font-semibold p-4 block"
+                    >
+                      ${result.value / 100}
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Container>
     </main>
   );
 };
